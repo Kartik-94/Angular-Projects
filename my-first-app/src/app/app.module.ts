8e6d435dd0ec55703a42ae581538b884fa2c1a91
kartik verma <kartikkuk@gmail.com>
2020-03-14 19:16:21 -0400

    if added, will allow app to use angular forms

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
