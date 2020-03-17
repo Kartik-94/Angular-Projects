4334d15bc941b04d4535129d7f60b05f054b41c5
Verma <Kartik.Verma@investorsgroup.com>
2020-03-17 17:41:14 -0400

    add set timeout function for add server button which dynamically enables btn after 2s

--- my-second-app/src/app/server/server.component.ts
+++ my-second-app/src/app/server/server.component.ts
@@ -7,11 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServerComponent implements OnInit {
   serverID: number = 10;
-  serverStatus: string = 'offline'; 
+  serverStatus: string = 'offline';
+  allowNewServer = true;
   constructor() { }
 
-
   ngOnInit(): void {
+  setTimeout(() =>{
+    this.allowNewServer = false;
+  },2000); 
   }
 
 }
